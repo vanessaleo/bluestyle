1e06fe7c8d287852979207d2017086ffea08e00d
vanessaleo <vanessaleo3952@gmail.com>
2017-04-28 19:34:16 +0200

    edit

--- client/trattamenti/trattamenti.controller.js
+++ client/trattamenti/trattamenti.controller.js
@@ -1,3 +1,67 @@
-angular.module('app').controller('TrattamentiCtrl',function(){
-  
+angular.module('app').controller('TrattamentiCtrl',function($scope){
+  $scope.getPiega = function(){
+    $scope.piega = true;
+    $scope.makeup=false;
+    $scope.raccolti = false;
+    $scope.taglio=false;
+    $scope.permanente=false;
+    $scope.meches=false;
+    $scope.colore=false;
+
+  }
+  $scope.getRaccolti = function(){
+    $scope.piega = false;
+    $scope.makeup=false;
+    $scope.raccolti = true;
+    $scope.taglio=false;
+    $scope.permanente=false;
+    $scope.meches=false;
+    $scope.colore=false;
+  }
+  $scope.getMakeup = function(){
+    $scope.piega = false;
+    $scope.makeup=true;
+    $scope.raccolti = false;
+    $scope.taglio=false;
+    $scope.permanente=false;
+    $scope.meches=false;
+    $scope.colore=false;
+  }
+  $scope.getTaglio = function(){
+    $scope.piega = false;
+    $scope.makeup=false;
+    $scope.raccolti = false;
+    $scope.taglio=true;
+    $scope.permanente=false;
+    $scope.meches=false;
+    $scope.colore=false;
+
+  }
+  $scope.getColore=function(){
+    $scope.piega = false;
+    $scope.makeup=false;
+    $scope.raccolti = false;
+    $scope.taglio=false;
+    $scope.permanente=false;
+    $scope.meches=false;
+    $scope.colore=true;
+  }
+  $scope.getPermanente=function(){
+    $scope.piega = false;
+    $scope.makeup=false;
+    $scope.raccolti = false;
+    $scope.taglio=false;
+    $scope.permanente=true;
+    $scope.meches=false;
+    $scope.colore=false;
+  }
+  $scope.getMeches=function(){
+    $scope.piega = false;
+    $scope.makeup=false;
+    $scope.raccolti = false;
+    $scope.taglio=false;
+    $scope.permanente=false;
+    $scope.meches=true;
+    $scope.colore=false;
+  }
 })
